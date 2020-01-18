1eacbaecdeaceb1fb221d8698dbc051f8c748673
Diego Espinoza <diespinozas@gmail.com>
2020-01-18 14:26:00 -0500

    create firebase config file and add initialize function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
