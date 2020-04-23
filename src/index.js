56e55d9dfd289ea44599a7378d9980737d855940
Diego Espinoza <diego.espinoza@buildingconnected.com>
2020-04-23 00:34:48 -0500

    change languaje for  antd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase';
-import { firebaseConfig } from './firebase.config';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
+import { firebaseConfig } from "./firebase.config";
+import { ConfigProvider } from "antd";
+import es_ES from "antd/es/locale/es_ES";
+import "antd/dist/antd.css";
 
 firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ConfigProvider locale={es_ES}>
+    <App />
+  </ConfigProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
